4c75c490481255263efa32599078a47df99a7375
unknown <pitermichkail@ymail.com>
2022-06-08 17:15:40 +0700

    form component

--- huft/src/app/todo-form/todo-form.component.ts
+++ huft/src/app/todo-form/todo-form.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { TodoListService } from '../todo-list/todo-list.service';
 
 @Component({
   selector: 'app-todo-form',
@@ -6,8 +17,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-form.component.scss']
 })
 export class TodoFormComponent implements OnInit {
+  validateForm!: FormGroup;
 
-  constructor() { }
+  submitForm(value: {
+    title: string,
+    completed: boolean
+  }): void {
+    for(const key in this.validateForm.controls) {
+      if(this.validateForm.controls.hasOwnProperty(key)) {
+        this.validateForm.controls[key].markAsDirty();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
+    value.completed = false;
+    this.validateForm.reset();
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
